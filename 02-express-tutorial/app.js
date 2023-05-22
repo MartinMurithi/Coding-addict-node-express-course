7dfc939a2c9263bd28610df31bc5af0b085c208c
MartinMurithi <wachiramartin442@gmail.com>
2023-05-22 19:28:37 +0300

    ..

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,17 +1,32 @@
-console.log("Express tutorial.....");
-
-
 const http = require("http");
 
 function customServer() {
-    const server = http.createServer((req, res) => {
-        console.log("LOL... Backend is not easy, but i must do it...");
-        res.writeHead(200, { "content-type": "text/html" });
-        res.write("<h1>Home Page</h1>");
-        res.end();
-    })
+  const server = http.createServer((req, res) => {
+      const url = req.url;
+      console.log(url);
+    if (url === "/") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>Home Page</h1>");
+      res.end();
+    }
+    else if (url === "/about") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>About</h1>");
+      res.end();
+    }
+    else if (url === "/contact") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>Contacts</h1>");
+      res.end();
+    }
+    else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("<h1> 404: Page not found!</h1>");
+      res.end();
+    }
+  });
 
-    server.listen(5000);
+  server.listen(5000);
 }
 
-customServer()
+customServer();
